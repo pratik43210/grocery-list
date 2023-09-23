54bb201b79b9332ae91741b67ac4ac17a5728e25
Pratik Raja <pratik.raja.3847@outlook.com>
2023-09-23 15:40:17 +0530

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@
 const Header=({title})=>{
   return (
     <header>
-        <h1 className="header">{title}</h1>
+        <h1 className="header"><img alt="app-icon" className="app-icon" src="https://cdn-icons-png.flaticon.com/512/3724/3724788.png"/>{title}</h1>
     </header>
   )
 }
